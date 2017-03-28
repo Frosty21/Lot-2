eebc4814860025b3316473c8b2bac807d2be31e2
Frosty21 <nathan.froese21@gmail.com>
2017-03-28 14:54:55 -0700

    add Gameloading image

--- client_screen/src/GameLoad.jsx
+++ client_screen/src/GameLoad.jsx
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
-
 export default class GameLoad extends Component {
+
   render() {
     return(
       <div>
         <h1> GameLoad </h1>
+        <Preload/>
       </div>
     )
   }
 }
+var Preload = React.createClass({
+  render: function(){
+    return(
+
+      <div id="global">
+
+        <div className="preloading-top mask">
+          <div className="plane"></div>
+        </div>
+        <div className="preloading-middle mask">
+          <div className="plane"></div>
+        </div>
+
+        <div className="preloading-bottom mask">
+          <div className="plane"></div>
+        </div>
+        <p className="preloading-text" ><i>LOADING...</i></p>
+
+      </div>
+    )
+  }
+})
