6386733d14b2af75e20d0412efb8349a0c592bd5
Robert Canas <robert_canas@hotmail.com>
2017-03-30 03:42:48 +0000

    Connecting room to client screen.

--- client_screen/src/GamePlay.jsx
+++ client_screen/src/GamePlay.jsx
@@ -5,59 +5,20 @@ export default class GamePlay extends Component {
     // Render Questions, Answers, Users, UserScore, UserAnswer
     return(
 
-    <section class="main">
-      <div class="jumbotron vertical-center">
-        <div class="container text-center">
+    <section className="main">
+      <div className="jumbotron vertical-center">
+        <div className="container text-center">
           <h1>Trivia101</h1>
         </div>
       </div>
 
-    <div class="timer-in-game">
-      <div class="panel panel-default">
-        <div class="panel-heading">Time Left</div>
-        <div class="panel-body">5 Seconds</div>
-        <div class="progress">
-          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%">
-            <span class="sr-only">50% Complete</span>
-          </div>
-        </div>
+    <div className="timer-in-game">
+      <div className="panel panel-default">
+        <div className="panel-heading">Time Left</div>
+        <div className="panel-body">5 Seconds</div>
       </div>
     </div>
 
-
-      <div class="users-in-lobby">
-
-      <div class="row">
-
-        <div class="col-sm-4">
-          <div class="card" style="width: 20rem;">
-            <div class="card-block">
-              <h4 class="card-name">Ermis</h4>
-              <p class="card-score">Score: 2/5</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="card" style="width: 20rem;">
-            <div class="card-block">
-              <h4 class="card-name">Rob</h4>
-              <p class="card-statistics">Score: 3/5</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="card" style="width: 20rem;">
-            <div class="card-block">
-              <h4 class="card-name">Nate</h4>
-              <p class="card-statistics">Score: 4/5</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      </div>
     </section>
     )
   }
