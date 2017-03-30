aa5b39cf93313b730a822d9c55193fe6f9f612da
Robert Canas <robert_canas@hotmail.com>
2017-03-30 18:43:48 +0000

    Moved client screen button to client mobile. Client screen gameplay template buttons added.

--- client_screen/src/GamePlay.jsx
+++ client_screen/src/GamePlay.jsx
@@ -8,16 +8,25 @@ export default class GamePlay extends Component {
     <section className="main">
       <div className="jumbotron vertical-center">
         <div className="container text-center">
-          <h1>Trivia101</h1>
+          <p>Scream for no reason at 4 am attack the dog then pretend like nothing happened chew foot ?</p>
         </div>
       </div>
 
-    <div className="timer-in-game">
       <div className="panel panel-default">
-        <div className="panel-heading">Time Left</div>
-        <div className="panel-body">5 Seconds</div>
+        <div className="panel-heading">Panel heading without title</div>
+        <div className="panel-body">
+          A: Answer A
+        </div>
+        <div className="panel-body">
+          B: Answer B
+        </div>
+        <div className="panel-body">
+          C: Answer C
+        </div>
+        <div className="panel-body">
+          D: Answer D
+        </div>
       </div>
-    </div>
 
     </section>
     )
