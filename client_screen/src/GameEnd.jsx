a12c639be707e5e71834a0c3b6969ac0b5901e16
Robert Canas <robert_canas@hotmail.com>
2017-04-06 08:47:36 +0000

    GameOverScreen

--- client_screen/src/GameEnd.jsx
+++ client_screen/src/GameEnd.jsx
@@ -1,43 +1,19 @@
 import React, {Component} from 'react';
-import endParty from './partyexplode-gameEnd.js';
 
 export default class GameEnd extends Component {
-  constructor() {
-    super();
-    this.state = {
-      loading: true
-    }
-  }
-
-  componentDidMount() {
-    if (this.state.loading){
-      endParty();
-    }
-  }
 
   render() {
     document.getElementById("background-screen").className = "background-GameEnd";
-    return(
+    return (
       <div>
         <h1> Game Over </h1>
-        <div className="winner">
-        <h2> The winner is ... </h2>
-        <div className="winnerScore">
-
+    {Object.keys(this.props.usersScores).map( (key)=>{
+      return (
+        <div>
+          <h2>  {key} : {this.props.usersScores[key]} </h2>
         </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-var PartyPreloader = React.createClass({
-  render: function(){
-    return(
-      <div>
-        <script>Partyexplode</script>
-        <canvas id="drawing_canvas"></canvas>
-      </div>
       )
-  }
-});
+    }).sort()}
+    </div>
+  )}
+}
